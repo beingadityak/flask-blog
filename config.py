9b233d6f428453bb9efb328fe10deb03f56a6ebc
beingadityak <me@beingadityak.com>
2023-10-08 20:06:40 +0530

    (feat): adds users and posts database

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://flaskblog:flaskblog@localhost/flaskblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
