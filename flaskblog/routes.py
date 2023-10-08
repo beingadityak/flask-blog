722fa20123e97389d78ae41b3f3ae692555a2787
beingadityak <me@beingadityak.com>
2023-10-08 10:07:47 +0530

    (feat): adds templates and env vars

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,18 @@
 from flaskblog import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'Aditya'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
